2b5bd9f7b9b2420e63cc2b7275dedcaf5d8936f3
kishanrajeev <kishanrj@outlook.com>
2023-08-09 00:17:48 -0400

    Added document functionality

--- main.py
+++ main.py
@@ -11,30 +11,36 @@ def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def new_palm():
-    print("Enter 'n' to reset the conversation and 'exit' to exit Palm. Limit 90 prompts per minute.")
-    Og_response = input("Start a new conversation: ")
-    if Og_response == 'n':
-        clear_console()
-        new_palm()
-    elif Og_response == 'exit':
-        exit()
-    else:
-        palm.configure(api_key=k2key.API_KEY)
+    document_content = input("Paste your entire document here: ")
+    while True:
+        print("At any point, type 'new' to paste a new document.")
+        Og_response = input("Would you like PalM to ask or answer your questions about the document?(ask/answer): ")
+        if Og_response == 'ask':
+            palm.configure(api_key=k2key.API_KEY)
 
-        response = palm.chat(messages=(Og_response), temperature = 1)
-        print(response.last)
-        while True:
-            Og2_response = input('Respond to Palm: ')
-            if Og2_response == 'n':
-                clear_console()
-                new_palm()
-            elif Og2_response == 'exit':
-                exit()
-            else:
-                response = response.reply(Og2_response)
-                print(response.last)
+            response = palm.chat(context=document_content, messages=(f'{Og_response} questions about the document provided.'), temperature=1)
+            print(response.last)
+
+        elif Og_response == 'answer':
+            question = input("What is your question?: ")
+
+            palm.configure(api_key=k2key.API_KEY)
+
+            response = palm.chat(context=document_content, messages=(f'{Og_response} questions about the document. Question: {question}'), temperature=1)
+            print(response.last)
+
+        elif Og_response == 'exit':
+            exit()
+        elif Og_response == 'new':
+            new_palm()
 
 
 if __name__ == '__main__':
-    while True:
         new_palm()
+
+
+
+
+
+
+
